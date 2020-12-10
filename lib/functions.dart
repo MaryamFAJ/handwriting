a3f645c4815f2448aaff50243ca1fc07aa3fbd82
Maryam Fajobi <maryam.fajobi@datawragling.io>
2020-12-10 10:21:05 +0100

    Update

--- lib/functions.dart
+++ lib/functions.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 
 Future<http.StreamedResponse> pic2text(File file) async {
   var url =
-      'http://handwriting-extraction.herokuapp.com/predict handwriting local images';
+      'https://handwriting-extraction.herokuapp.com/docs#/default/predict__predict_handwriting_local_images_post';
   var request = http.MultipartRequest('POST', Uri.parse(url));
   request.headers['Accept'] = 'application/json';
   request.headers['Content-Type'] = 'multipart/form-data';
